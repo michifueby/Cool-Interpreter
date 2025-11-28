21f84f5b8e270b31de5163d391812b48f915fb64
michifueby <michi.fueby@gmail.com>
2025-11-28 17:09:14 +0100

    feat: Update project draft

--- src/Cool.Interpreter/Cool.Interpreter.Lib/Language/Interpretation/InterpretationResult.cs
+++ src/Cool.Interpreter/Cool.Interpreter.Lib/Language/Interpretation/InterpretationResult.cs
@@ -1,16 +1,17 @@
 //-----------------------------------------------------------------------
-// <copyright file="CoolInterpreter.cs" company="FH Wiener Neustadt">
+// <copyright file="InterpretationResult.cs" company="FH Wiener Neustadt">
 //     Copyright (c) FH Wiener Neustadt. All rights reserved.
 // </copyright>
 // <author>Michael Füby, Armin Zimmerling, Mahmoud Ibrahim</author>
-// <summary>CoolInterpreter</summary>
+// <summary>InterpretationResult</summary>
 //-----------------------------------------------------------------------
 
 namespace Cool.Interpreter.Lib.Language.Interpretation;
 
 /// <summary>
-/// Executes Cool programs by directly interpreting the Cool program.
+/// Represents the complete result of executing a Cool program via <see cref="CoolInterpreter"/>.
+/// This is the one and only return type from the public facade.
 /// </summary>
-public class CoolInterpreter : IInterpreter
+public class InterpretationResult
 {
 }
